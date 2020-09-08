49fb63edcf697524bf027b60e45527b1475b363a
Dat Tran <trduydat@gmail.com>
2020-09-08 17:15:12 +0700

    Update DuyAnh's dataset and refactor project

--- stack/test.py
+++ stack/test.py
@@ -1,14 +1,15 @@
 import json
 
-with open(r'TuanAnh.json', 'r') as dt:
+with open(r'DatTran.json', 'r') as dt:
 	data = json.load(dt)
 dataReQ= []
 dataReA= []
 for dataLine in data:
 	dataReQ.append(dataLine['question'])
 	dataReA.append(dataLine['answer'])
-with open('TuanAnh.txt', 'w') as re:
-	for count in range(len(dataReQ)-1):
-		re.write(dataReQ[count] + "\n```" + dataReQ[count+1] + "\n")
+count = 0
+with open('./txt/DatTran.txt', 'w') as re:
+	while count < len(dataReQ)-1:
+		re.write(dataReQ[count] + "\n```" + dataReQ[count+1] + "\n\n")
 		re.write(dataReA[count] + "\n```" + dataReA[count+1] + "\n\n\n\n")
-		count +=1
+		count +=2
